52b26bd42bce0d410b851e9e3289c951dbd619c9
Sharat Akasapu <sharataka@gmail.com>
2013-03-06 14:40:40 -0800

    added validations to project name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,15 +6,16 @@ class ProjectsController < ApplicationController
 		@project = Project.new
 	end
 
-	def create
- 		@project = Project.new(params[:project])
-  		if @project.save
-   			flash[:notice] = "Project has been created."
-    		redirect_to @project
-  		else
-    		# nothing, yet
-  		end
-	end
+def create
+  @project = Project.new(params[:project])
+  if @project.save
+    flash[:notice] = "Project has been created."
+    redirect_to @project
+  else
+    flash[:alert] = "Project has not been created."
+    render :action => "new"
+end
+end
 
 	def show
  		@project = Project.find(params[:id])
